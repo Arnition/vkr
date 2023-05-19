30710363d242a7c0b425acb9777c2af3ee5f127f
Arnition <drevil72123@mail.ru>
2023-05-19 16:48:08 +0300

    vkr

--- app/src/main/java/com/example/vkrapp/feed_activity.java
+++ app/src/main/java/com/example/vkrapp/feed_activity.java
@@ -2,34 +2,73 @@ package com.example.vkrapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import java.util.List;
+
 
 public class feed_activity extends AppCompatActivity {
 
+    ListView userList;
+    TextView header;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    SimpleCursorAdapter userAdapter;
+
     Spinner myspinner;
+    Spinner myspinnerTest;
     ArrayAdapter<CharSequence> adapter;
+    ArrayAdapter<CharSequence> adapter2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
+
         myspinner = (Spinner) findViewById(R.id.spinner);
-        adapter = ArrayAdapter.createFromResource(this,R.array.activity_arr, R.layout.spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.activity_arr, R.layout.spinner);
         myspinner.setAdapter(adapter);
 
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown);
+        databaseHelper = new DatabaseHelper(getApplicationContext());
 
-        DBAdapter db = new DBAdapter(this);
-        db.open();
-        db.close();
+        myspinnerTest = (Spinner) findViewById(R.id.spnTest);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // открываем подключение
+        db = databaseHelper.getReadableDatabase();
 
-        Toast.makeText(this,"База данных подключена!", Toast.LENGTH_SHORT);
+        //получаем данные из бд в виде курсора
+        userCursor =  db.rawQuery("select * from "+ DatabaseHelper.TABLE, null);
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[] {DatabaseHelper.COLUMN_NAME};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item,
+                userCursor, headers, new int[]{android.R.id.text1}, 0);
+        myspinnerTest.setAdapter(userAdapter);
     }
 
-    public void onRadioButtonClicked(View view) {
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        // Закрываем подключение и курсор
+        db.close();
+        userCursor.close();
     }
 }
